4c18b0de20803512e7e653d90a73eb577b8326e1
Kostas Spyropoulos <inigo.aldana@gmail.com>
2010-11-08 18:42:24 -0500

    work in progress, doesn't compile

--- src/eu/spyropoulos/android/oldweather/Connection.java
+++ src/eu/spyropoulos/android/oldweather/Connection.java
@@ -6,10 +6,12 @@ import android.util.Log;
 
 public class Connection {
 
+    private DefaultHttpClient mHttpClient;
+
     /**
-     * Check to see if we there is network connection
+     * Check to see if there is network connection.
      *
-     * @param context The base context passed from an activity
+     * @param context The base context passed from an activity 
      * @return True is connected or connecting, false otherwise
      */
     public static boolean isOnline(Context context) {
@@ -20,6 +22,26 @@ public class Connection {
         } else {
             return false;
         }
+    }
+
+    public Connection() {
+        mHttpClient = new DefaultHttpClient();
+    }
+
+    /**
+     * Login to server.
+     *
+     * @param login
+     * @param password
+     */
+    public boolean login(String login, String password) {
+        HttpPost httpPost = new HttpPost("https://login.zooniverse.org/login?service=http%3A%2F%2Fwww.oldweather.org%2Fclassify");
+
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+        nvps.add(new BasicNameValuePair("username", login));
+        nvps.add(new BasicNameValuePair("password", password));
+        nvps.add(new BasicNameValuePair("service", "http://www.oldweather.org/classify"));
+        
     }
 }
 
--- src/eu/spyropoulos/android/oldweather/OldWeatherApp.java
+++ src/eu/spyropoulos/android/oldweather/OldWeatherApp.java
@@ -1,23 +1,71 @@
 package eu.spyropoulos.android.oldweather;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.preference.PreferenceManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
+
+
 public class OldWeatherApp extends Activity {
+    private static final String TAG = "OldWeather";
+
+    private DbAdapter mDb;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
+        mDb = new DbAdapter(this);
+        mDb.open();
+
+        // Check if first run
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getBoolean("first_run", true)) {
+            resetSharedPreferences(prefs);
+        }
+
+        mDb.createUser("iniju", null);
+
         Toast netStatusMessage = null;
+        Cursor cur = mDb.fetchUser("iniju");
+        String pwd = cur.getString(1);
+        if (pwd == null) {
+            netStatusMessage = Toast.makeText(OldWeatherApp.this, "String was null", Toast.LENGTH_SHORT);
+        } else {
+            netStatusMessage = Toast.makeText(OldWeatherApp.this, "String was not null, it was " + pwd, Toast.LENGTH_SHORT);
+        }
+        netStatusMessage.show();
+
+
+        netStatusMessage = null;
         if (Connection.isOnline(getBaseContext())) {
             netStatusMessage = Toast.makeText(OldWeatherApp.this, "Network is on", Toast.LENGTH_SHORT);
         } else {
             netStatusMessage = Toast.makeText(OldWeatherApp.this, "Network is off", Toast.LENGTH_SHORT);
         }
         netStatusMessage.show();
+    }
+
+    /**
+     * Initialise shared preferences
+     */
+    private void resetSharedPreferences(SharedPreferences prefs) {
+        SharedPreferences.Editor editor = prefs.edit();
+
+        // Delete all, if any, shared preferences
+        editor.clear();
+
+        // Set default values for preferences
+        editor.putBoolean("first_run", false);
+        editor.putBoolean("auto_login", false);
+        editor.putString("last_login", "");
+
+        // Save preferences
+        editor.commit();
     }
 }
 
